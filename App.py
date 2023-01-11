668b5fe98f233c2c50494b9a60d27945b2e31175
Guy Donagi <107021187+ImDonagi@users.noreply.github.com>
2023-01-11 09:40:33 +0200

    Initializing the app

---
+++ App.py
@@ -0,0 +1,58 @@
+# imports
+import streamlit as st
+import cv2
+import numpy as np
+import skimage.io as io
+
+# ----------------------------
+
+# Functions:
+def segment_image_kmeans(img, k=3, attempts=10): 
+
+    # Convert MxNx3 image into Kx3 where K=MxN
+    pixel_values  = img.reshape((-1,3))  #-1 reshape means, in this case MxN
+
+    #We convert the unit8 values to float as it is a requirement of the k-means method of OpenCV
+    pixel_values = np.float32(pixel_values)
+
+    # define stopping criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+    
+    _, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, attempts, cv2.KMEANS_RANDOM_CENTERS)
+    
+    # convert back to 8 bit values
+    centers = np.uint8(centers)
+
+    # flatten the labels array
+    labels = labels.flatten()
+    
+    # convert all pixels to the color of the centroids
+    segmented_image = centers[labels.flatten()]
+    
+    # reshape back to the original image dimension
+    segmented_image = segmented_image.reshape(img.shape)
+    
+    return segmented_image
+# ----------------------------
+
+# Interface:
+
+header = st.container()
+input = st.container()
+output = st.container()
+
+with header:
+    st.title("Area Calculating Using AruCo Markers")
+    st.text("This small project was built as a part of \"Intro To Image Processing\"\ncourse in the Faculty of Agriculture.\nIt's quite simple:\n")
+    st.text("*  Upload an image containing an object and an AruCo marker to the\n \"Input Image\" section.")
+    st.text("*  The area of the object will be presented in the \"Calculated Area\" section.")
+
+with input:
+    st.header("Input Values:")
+
+    sel_col = st.container()
+    disp_col = st.container()
+
+
+with output:
+    st.header("Calculated Area:")
